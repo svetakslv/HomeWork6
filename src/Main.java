ef03e314044b76b04aeea61f02c3a210acbdd35a
Светка <sveta.chakrygina@mail.ru>
2022-08-29 14:00:01 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,50 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ///  int[] arr = new int[30];
-        ///  int sum = 0;
-        ///  java.util.Random random = new java.util.Random();
-        ///  arr[sum] = random.nextInt(100_000) + 100_000;
-        /// for (int element : arr) {
-        ///         sum += element;
-        ///      System.out.println(sum);
 
         ///Задача №1
+        Random random = new Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
-            Random random = new Random();
-            arr[i] = random.nextInt(100_000) + 100_000;
-          ///  for (int index = 0; index < arr.length; index++) {
-           ///     arr[index] = index + 1;
-            ///    System.out.print(arr[index] + " ");
-
-                Collections.shuffle(Arrays.asList(arr));
-                System.out.print(Arrays.toString(arr) + " ");
-
+            if (i == arr.length - 1) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+                arr[0] = random.nextInt(100_000) + 100_000;
+                arr[1] = random.nextInt(100_000) + 100_000;
+                arr[2] = random.nextInt(100_000) + 100_000;
+                arr[3] = random.nextInt(100_000) + 100_000;
+                arr[4] = random.nextInt(100_000) + 100_000;
+                arr[5] = random.nextInt(100_000) + 100_000;
+                arr[6] = random.nextInt(100_000) + 100_000;
+                arr[7] = random.nextInt(100_000) + 100_000;
+                arr[8] = random.nextInt(100_000) + 100_000;
+                arr[9] = random.nextInt(100_000) + 100_000;
+                arr[10] = random.nextInt(100_000) + 100_000;
+                arr[11] = random.nextInt(100_000) + 100_000;
+                arr[12] = random.nextInt(100_000) + 100_000;
+                arr[13] = random.nextInt(100_000) + 100_000;
+                arr[14] = random.nextInt(100_000) + 100_000;
+                arr[15] = random.nextInt(100_000) + 100_000;
+                arr[16] = random.nextInt(100_000) + 100_000;
+                arr[17] = random.nextInt(100_000) + 100_000;
+                arr[18] = random.nextInt(100_000) + 100_000;
+                arr[19] = random.nextInt(100_000) + 100_000;
+                arr[20] = random.nextInt(100_000) + 100_000;
+                arr[21] = random.nextInt(100_000) + 100_000;
+                arr[22] = random.nextInt(100_000) + 100_000;
+                arr[23] = random.nextInt(100_000) + 100_000;
+                arr[24] = random.nextInt(100_000) + 100_000;
+                arr[25] = random.nextInt(100_000) + 100_000;
+                arr[26] = random.nextInt(100_000) + 100_000;
+                arr[27] = random.nextInt(100_000) + 100_000;
+                arr[28] = random.nextInt(100_000) + 100_000;
+                arr[29] = random.nextInt(100_000) + 100_000;
             }
         }
+        Collections.shuffle(Arrays.asList(arr));
+        System.out.println(Arrays.toString(arr) + " ");
     }
+}
+
 
 
 
