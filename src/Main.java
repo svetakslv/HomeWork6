5bb3666652f7ff7da40b00389c59e3c6e516f684
Светка <sveta.chakrygina@mail.ru>
2022-08-30 15:48:08 +0300

    Задача №1,2,3,4(исправлено)

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,20 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
 
 
 public class Main {
-
-
     public static void main(String[] args) {
 
-
-        ///Задача №1,2,3
-
-        Random random = new Random();
-        int[] arr = new int[30];
+    ///Задача №1,2,3
+        int[] arr = generateRandomArray();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
             int num1 = arr[0] / arr.length;
             int num2 = arr[2] / arr.length;
             int divSum = sum / arr.length;
-
-            ///if (i == arr.length - 1) {
-            ///arr[i] = random.nextInt(100_000) + 100_000;
-            arr[0] = random.nextInt(100_000) + 100_000;
-            arr[1] = random.nextInt(100_000) + 100_000;
-            arr[2] = random.nextInt(100_000) + 100_000;
-            arr[3] = random.nextInt(100_000) + 100_000;
-            arr[4] = random.nextInt(100_000) + 100_000;
-            arr[5] = random.nextInt(100_000) + 100_000;
-            arr[6] = random.nextInt(100_000) + 100_000;
-            arr[7] = random.nextInt(100_000) + 100_000;
-            arr[8] = random.nextInt(100_000) + 100_000;
-            arr[9] = random.nextInt(100_000) + 100_000;
-            arr[10] = random.nextInt(100_000) + 100_000;
-            arr[11] = random.nextInt(100_000) + 100_000;
-            arr[12] = random.nextInt(100_000) + 100_000;
-            arr[13] = random.nextInt(100_000) + 100_000;
-            arr[14] = random.nextInt(100_000) + 100_000;
-            arr[15] = random.nextInt(100_000) + 100_000;
-            arr[16] = random.nextInt(100_000) + 100_000;
-            arr[17] = random.nextInt(100_000) + 100_000;
-            arr[18] = random.nextInt(100_000) + 100_000;
-            arr[19] = random.nextInt(100_000) + 100_000;
-            arr[20] = random.nextInt(100_000) + 100_000;
-            arr[21] = random.nextInt(100_000) + 100_000;
-            arr[22] = random.nextInt(100_000) + 100_000;
-            arr[23] = random.nextInt(100_000) + 100_000;
-            arr[24] = random.nextInt(100_000) + 100_000;
-            arr[25] = random.nextInt(100_000) + 100_000;
-            arr[26] = random.nextInt(100_000) + 100_000;
-            arr[27] = random.nextInt(100_000) + 100_000;
-            arr[28] = random.nextInt(100_000) + 100_000;
-            arr[29] = random.nextInt(100_000) + 100_000;
-
             if (sum > 0) {
+
                 Collections.shuffle(Arrays.asList(arr));
                 /// System.out.println(Arrays.toString(arr) + " ");
                 System.out.println("1. Сумма трат за месяц составила " + sum + " рублей.");
@@ -62,16 +22,25 @@ public class Main {
                 if (num1 != 0 && num2 != 0) {
 
                     System.out.println("2. Максимальная сумма трат за день составила " + Math.max(num1, num2) + " рублей. Минимальная сумма трат за день составила " + Math.min(num1, num2) + " рублей.");
-                    System.out.println("3. Средняя сумма трат за месяц составила " + divSum + "рублей.");
+                    System.out.println("3. Средняя сумма трат за месяц составила " + divSum + " рублей.");
+
+                    ///Задача №4
+                    char[] reverseFullName = new char[]{'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+                    for (int j = reverseFullName.length - 1; j >= 0; j--) {
+                        System.out.print(reverseFullName[j]);
+                    }
                     break;
                 }
             }
         }
-        ///Задача №4
-        char[] reverseFullName = new char[]{'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-                System.out.print(reverseFullName[i]);
+    }
+    public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] array = new int[30];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt(100_000) + 100_000;
             }
+            return array;
         }
     }
 
