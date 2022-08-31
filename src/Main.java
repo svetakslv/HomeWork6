904542f6522fcbc0494c23fc9481e1c3cceba92d
Светка <sveta.chakrygina@mail.ru>
2022-08-31 20:20:21 +0300

    Задача №1,2,3,4(исправлено)

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,31 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-
         ///Задача №1,2,3
         int[] arr = generateRandomArray();
+
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-            int num1 = arr[0] / arr.length;
-            int num2 = arr[2] / arr.length;
-            int divSum = sum / arr.length;
-            if (sum > 0) {
+        for (int k : arr) {
+            sum += k;
+        }
+        System.out.println("1. Сумма трат за месяц составила " + sum + " рублей.");
 
-                System.out.println("1. Сумма трат за месяц составила " + sum + " рублей.");
-
-                if (num1 != 0 && num2 != 0) {
-
-                    System.out.println("2. Максимальная сумма трат за день составила " + Math.max(num1, num2) + " рублей. Минимальная сумма трат за день составила " + Math.min(num1, num2) + " рублей.");
-                    System.out.println("3. Средняя сумма трат за месяц составила " + divSum + " рублей.");
-
-                    break;
-                }
+        int min = arr[0];
+        int max = arr[0];
+        for (int k : arr) {
+            if (min > k) {
+                min = k;
+            } else if (max < k) {
+                max = k;
             }
         }
+        System.out.printf("2. Минимальная сумма трат за день составила %s рублей.\n" + "Максимальная сумма трат за день составила %s рублей.\n", min, max);
+
+        double divSum = (double) sum / arr.length;
+        String average = String.format("%.2f", divSum);
+
+        System.out.println("3. Средняя сумма трат за месяц составила " + average + " рублей.");
+
         ///Задача №4
         char[] reverseFullName = new char[]{'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int j = reverseFullName.length - 1; j >= 0; j--) {
@@ -42,6 +45,8 @@ public class Main {
         return array;
     }
 }
+
+
 
 
 
